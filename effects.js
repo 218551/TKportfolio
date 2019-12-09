427b5a47341068832e42d5bcfa8784f89c75cbf0
tomas <tomaszxkubat@gmail.com>
2019-12-09 21:04:25 +0100

    transition repaired

--- effects.js
+++ effects.js
@@ -3,18 +3,18 @@ $(document).ready(animationStartShow);
 $(document).ready(function () {
 
     $('#startTab').click(function () {
-        if(!($('#contentStart').is(':visible'))) {
+        if(!($('#contentStart').is(':visible')))
+            setTimeout(animationStartShow,1000);
+        if($('#contentAbout').is(':visible'))
             animationAboutHide();
-            animationStartShow();
-        }
-    })
+    });
 
     $('#aboutTab').click(function () {
-        if(!($('#contentAbout').is(':visible'))) {
+        if(!($('#contentAbout').is(':visible')))
+            setTimeout(animationAboutShow,1000);
+        if($('#contentStart').is(':visible'))
             animationStartHide();
-            animationAboutShow();
-        }
-    })
+    });
 
 });
 
@@ -22,47 +22,49 @@ $(document).ready(function () {
 
 function animationStartShow(){
     $('#contentStart').show();
-    $('#moustache1').show(1000).animate({"top":"-=35px"},500).animate({"left":"-=210px"},500);
-    $('#moustache2').show(1000).animate({"top":"+=35px"},500).animate({"left":"+=210px"},500);
-    $('#myphoto').fadeTo(1000,1)
+    $('#moustache1').show(500).animate({"top":"-=35px"},500).animate({"left":"-=210px"},500);
+    $('#moustache2').show(500).animate({"top":"+=35px"},500).animate({"left":"+=210px"},500);
+
+    setTimeout(function () {
+        $('#myphoto').fadeTo(500,1);
+    }, 1500);
 
     setTimeout(function(){
-        $('#welcome1').fadeTo(1000, 1);
-    }, 2000);
+        $('#welcome1').fadeTo(500, 1);
+    }, 1500);
 
     setTimeout(function(){
-        $('#welcome2').fadeTo(1000, 1);
-        $('#welcome3').fadeTo(1000, 1);
-    }, 3000);
+        $('#welcome2').fadeTo(500, 1);
+        $('#welcome3').fadeTo(500, 1);
+    }, 2500);
 }
 
 function animationStartHide(){
-    $('#contentStart').hide(1000);
-    $('#moustache1').animate({"top":"+=35px"},500).animate({"left":"+=210px"},500);
-    $('#moustache2').animate({"top":"-=35px"},500).animate({"left":"-=210px"},500);
-    $('#moustache1').hide(1000)
-    $('#moustache2').hide(1000)
-    $('#myphoto').fadeTo(1000,0)
+    setTimeout(function () {
+        $('#contentStart').hide(200);
+    },800);
 
-    setTimeout(function(){
-        $('#welcome1').fadeTo(1000, 0);
-    }, 0);
+    $('#moustache1').animate({"top":"+=35px"},400).animate({"left":"+=210px"},400);
+    $('#moustache2').animate({"top":"-=35px"},400).animate({"left":"-=210px"},400);
+    $('#welcome1').fadeTo(400, 0);
+    $('#welcome2').fadeTo(400, 0);
+    $('#welcome3').fadeTo(400, 0);
+    $('#myphoto').fadeTo(1000,0);
+    setTimeout(function () {
+        $('#moustache1').hide(200);
+        $('#moustache2').hide(200);
+    }, 800);
 
-    setTimeout(function(){
-        $('#welcome2').fadeTo(1000, 0);
-        $('#welcome3').fadeTo(1000, 0);
-    }, 0);
 }
 
 function animationAboutShow(){
     $('#contentAbout').show();
-
-    setTimeout(function(){
-        $('#contentAbout').fadeTo(1000,1);
-    }, 1000);
+    $('#contentAbout').fadeTo(500,1);
 }
 
 function animationAboutHide(){
-    $('#contentAbout').hide(1000);
-    $('#contentAbout').fadeTo(1000,0);
+    setTimeout(function () {
+        $('#contentAbout').hide();
+    },500);
+    $('#contentAbout').fadeTo(500,0);
 }
